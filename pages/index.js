b208c0ce3104770026da083006c37a9b4f6eddd2
Burak Saraloglu <buraksaraloglu1@gmail.com>
2021-05-31 21:20:16 +0300

    resolve conflict

--- pages/index.js
+++ pages/index.js
@@ -6,29 +6,27 @@ const { Content } = Layout;
 
 import Logo from '../components/Logo';
 import LogoSpinner from '../components/LogoSpinner';
+import { CollectionsPage } from '../components/try';
 import useStyles from './styles';
 
 export default function Home() {
-  const classes = useStyles();
-  return (
-    <>
-      <Head>
-        <title>Tradegraf</title>
-        <meta name="description" content="Tradegraf" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Layout>
-        <Content>
-          <Space
-            direction="vertical"
-            size="large"
-            className={classes.container}
-          >
-            <Logo />
-            <Button type="primary">Login</Button>
-          </Space>
-        </Content>
-      </Layout>
-    </>
-  );
+	const classes = useStyles();
+	return (
+		<>
+			<Head>
+				<title>Tradegraf</title>
+				<meta name="description" content="Tradegraf" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Layout>
+				<Content>
+					<Space direction="vertical" size="large" className={classes.container}>
+						<Logo />
+						<CollectionsPage />
+						<Button type="primary">Login</Button>
+					</Space>
+				</Content>
+			</Layout>
+		</>
+	);
 }
