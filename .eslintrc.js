b615ac9c8bdbb34b4e920894d5aecb81291e636a
Burak Saraloglu <buraksaraloglu1@gmail.com>
2021-06-27 18:18:43 +0300

    refactor: add react version to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -68,6 +68,11 @@ module.exports = {
 		'no-unused-vars': 'warn',
 	},
 	settings: {
+		react: {
+			pragma: 'React', // Pragma to use, default to "React"
+			fragment: 'Fragment', // Fragment to use (may be a property of <pragma>), default to "Fragment"
+			version: 'detect', // React version. "detect" automatically picks the version you have installed.
+		},
 		'import/resolver': {
 			webpack: {
 				config: './internals/webpack/webpack.prod.babel.js',
