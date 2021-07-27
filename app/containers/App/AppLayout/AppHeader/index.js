28c89adf811794f2f25ef196b927b67c206f57ae
MIXAS <umutefekorkmaz00@gmail.com>
2021-07-27 17:07:13 +0300

    task/APH-11 added Header Menu

--- app/containers/App/AppLayout/AppHeader/index.js
+++ app/containers/App/AppLayout/AppHeader/index.js
@@ -1,35 +1,51 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Layout, Menu, Dropdown, Button, Row, Col } from 'antd';
+import { Layout, Menu, Dropdown, Button } from 'antd';
 import _ from 'lodash';
-import { LogoutOutlined } from '@ant-design/icons';
+import {
+	LogoutOutlined,
+	UserAddOutlined,
+	ShoppingOutlined,
+	TeamOutlined,
+	UserOutlined,
+	ApiOutlined,
+} from '@ant-design/icons';
 import { useTranslation } from 'react-i18next';
-
 import Logo from '@app/components/Logo';
-
-import { getLangKey } from '@app/i18n';
 import routes from '@app/shared/routes';
 import { useAuth } from '@app/shared/hooks/useAuth';
 import useStyles from './styles';
 
 const { Header } = Layout;
-const { Item, Divider } = Menu;
+const { Divider } = Menu;
 
-const AppHeader = (properties) => {
-	const { t } = useTranslation();
+const AppHeader = () => {
+	const { t } = useTranslation('menu');
 	const classes = useStyles();
-	const { user, logout } = useAuth();
+	const { logout } = useAuth();
 
-	const userName = _.get(user, 'name', '');
-
-	const menu = (
+	const headMenu = () => (
 		<Menu>
-			<Item key="Item-1">{userName}</Item>
-
+			<Menu.Item key="1" icon={<UserAddOutlined />}>
+				{t('PROFILE_MANAGEMENT')}
+				<Link to="./profile/overview" />
+			</Menu.Item>
+			<Menu.Item key="2" icon={<ApiOutlined />}>
+				<Link to="./profile/api" />
+				{t('API_MANAGEMENT')}
+			</Menu.Item>
+			<Menu.Item key="3" icon={<ShoppingOutlined />}>
+				<Link to="./profile/subscription" />
+				{t('SUBSCRIPTION')}
+			</Menu.Item>
+			<Menu.Item key="4" icon={<TeamOutlined />}>
+				<Link to="./profile/invite" />
+				{t('INVITE')}
+			</Menu.Item>
 			<Divider />
-			{/* <Item key="Item-4" onClick={logoutRequest}>
-				<LogoutOutlined size="large" /> {t('LOGOUT')}
-			</Item> */}
+			<Menu.Item key="5" icon={<LogoutOutlined />} onClick={logout} danger>
+				{t('LOGOUT')}
+			</Menu.Item>
 		</Menu>
 	);
 
@@ -43,16 +59,9 @@ const AppHeader = (properties) => {
 			<Link to={routes.get('DASHBOARD').path}>
 				<Logo size="small" />
 			</Link>
-			{/* <div className={classes.userMenu}>
-				<Dropdown overlay={menu} placement="bottomLeft">
-					<Button type="primary" shape="circle" size="large" className={classes.userButton}>
-						{userName.charAt(0)}
-					</Button>
-				</Dropdown>
-			</div> */}
-			<Button icon={<LogoutOutlined />} onClick={logout}>
-				Logout
-			</Button>
+			<Dropdown overlay={headMenu}>
+				<Button icon={<UserOutlined />} placement="bottomRight" />
+			</Dropdown>
 		</Header>
 	);
 };
